8b42695f75dea06ca30b37484308795632a4694d
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2025-06-01 05:02:17 -0700

    added initial server side code. imported cors and created server router.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import apiRouter from './api/index.js'
+
 const app = express();
+app.use(cors({origin: ['http://localhost:3000']}))
+app.use(express.json());
+
+app.use('/api', apiRouter);
 
 
 app.listen(process.env.PORT,() => {
