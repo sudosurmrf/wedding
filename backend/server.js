2d26cf5a14904aa5aefc715a7d5f46102b12ba34
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2025-06-02 18:32:56 -0700

    added correct es module dirname import

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import apiRouter from './api/index.js'
 import path from 'path'
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
-app.use(cors({origin: ['http://localhost:5173']}))
+app.use(cors({origin: ['http://localhost:5173', 'https://were-so-back.com', 'https://defund-the-hoa.com']}))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
