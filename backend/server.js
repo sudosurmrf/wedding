ece06f68742c00c27361662bcc9894365efc84ba
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2025-06-02 18:28:02 -0700

    added server routing to dist folder

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import apiRouter from './api/index.js'
+import path from 'path'
 
 const app = express();
 app.use(cors({origin: ['http://localhost:5173']}))
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 app.use('/api', apiRouter);
 
