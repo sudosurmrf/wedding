5bfe2348e8a2508e08f20a1c57664ade5e597942
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2025-06-06 05:13:08 -0700

    mms.js update to use objectKey to make a filename

--- backend/api/mms.js
+++ backend/api/mms.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import prisma from '../prisma/index.js';
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import crypto from 'crypto';
+
 const router = express.Router();
 router.use(express.json());
 
@@ -10,76 +11,88 @@ const s3 = new S3Client({
   region: 'us-east-1',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
-const S3_BUCKET = process.env.S3_BUCKET
+const S3_BUCKET = process.env.S3_BUCKET;
 
-router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.urlencoded({ extended: false }));
 
-router.post('/', async(req,res,next) => {
-  try{
+router.post('/', async (req, res, next) => {
+  try {
     const fromNumber = req.body.From;
     const textBody = req.body.Body;
     const numMedia = parseInt(req.body.NumMedia, 10);
 
-     if (numMedia > 0) {
-     // Loop over each media attachment
-     for (let i = 0; i < numMedia; i++) {
-       const mediaUrl      = req.body[`MediaUrl${i}`];       // temporary media URL
-       const contentType   = req.body[`MediaContentType${i}`]; // "image/jpeg"
+    if (numMedia > 0) {
+      // Loop over each media attachment
+      for (let i = 0; i < numMedia; i++) {
+        const mediaUrl = req.body[`MediaUrl${i}`];
+        const contentType = req.body[`MediaContentType${i}`]; //"image/jpeg"
 
-       // binary data from the Twilio media URL gets downloaded
-       const authHeader  = Buffer.from(`${process.env.TWILIO_ACCOUNT_SID}:${process.env.TWILIO_AUTH_TOKEN}`).toString('base64');
+        //binary data from the Twilio media URL
+        const authHeader = Buffer.from(
+          `${process.env.TWILIO_ACCOUNT_SID}:${process.env.TWILIO_AUTH_TOKEN}`
+        ).toString('base64');
 
-      const mediaResponse = await fetch(mediaUrl, {
-        headers: { Authorization: `Basic ${authHeader}` },
+        const mediaResponse = await fetch(mediaUrl, {
+          headers: { Authorization: `Basic ${authHeader}` },
+        });
+        const arrayBuffer = await mediaResponse.arrayBuffer();
+        const fileBuffer = Buffer.from(arrayBuffer);
+
+        // Generate a unique key for S3
+        const fileExtension = contentType.split('/')[1]; //"jpeg", "png", "mp4"
+        const objectKey = `memories/${Date.now()}_${crypto
+          .randomBytes(8)
+          .toString('hex')}.${fileExtension}`;
+
+        // Upload to S3 
+        const putCommand = new PutObjectCommand({
+          Bucket: S3_BUCKET,
+          Key: objectKey,
+          Body: fileBuffer,
+          ContentType: contentType,
+        });
+        await s3.send(putCommand);
+
+        // construct the public URL
+        const publicUrl = `https://${S3_BUCKET}.s3.amazonaws.com/${objectKey}`;
+
+        //metadata to the database
+        await prisma.media.create({
+          data: {
+            fromNumber,
+            mediaUrl: publicUrl,
+            mediaType: contentType,
+          },
+        });
+      }
+    } else {
+      // plain text sms db save
+      const bodyText = textBody || '';
+      await prisma.message.create({
+        data: {
+          fromNumber,
+          body: bodyText,
+        },
       });
-      //convert response into an ArrayBuffer, then into a Buffer
-      const arrayBuffer = await mediaResponse.arrayBuffer();
-      const fileBuffer = Buffer.from(arrayBuffer);
+    }
 
-      // send to S3
-      const putCommand = new PutObjectCommand({
-        Bucket: S3_BUCKET,
-        Key: objectKey,
-        Body: fileBuffer,
-        ContentType: contentType,
-      });
-      await s3.send(putCommand);
+    // twilio reply afterwards, if i can finish setting it up
+    res.set('Content-Type', 'text/xml');
+    return res.send(
+      `<Response><Message>Thanks for sharing your memory! 💕</Message></Response>`
+    );
+  } catch (err) {
+    console.error('Error handling /mms:', err);
+    // Twilio expects a 200 OK even on failure, else it retries
+    res.set('Content-Type', 'text/xml');
+    return res.send(
+      `<Response><Message>Sorry, something went wrong. 😢</Message></Response>`
+    );
+  }
+});
 
-       //construct the public URL
-       const publicUrl = `https://${S3_BUCKET}.s3.amazonaws.com/${objectKey}`;
-
-       // saves media metadata to our db for sorting 
-       await prisma.media.create({
-         data: {
-           fromNumber,
-           mediaUrl: publicUrl,
-           mediaType: contentType,
-         },
-       });
-     }
-   } else {
-     // if just a plain text message
-     const bodyText = textBody || '';
-     await prisma.message.create({
-       data: {
-         fromNumber,
-         body: bodyText,
-       },
-     });
-   }
-
-   // tries to autoreply, although it might not work rn idt. 
-   res.set('Content-Type', 'text/xml');
-   return res.send(`<Response><Message>Thanks for sharing your memory! 💕</Message></Response>`);
- } catch (err) {
-   console.error('Error handling /mms:', err);
-   // twilio will retry the send if it does not recieve 200. 
-   res.set('Content-Type', 'text/xml');
-   return res.send(`<Response><Message>Sorry, something went wrong. 😢</Message></Response>`);
- }
-})
 export default router;
