a1118b32decdeeea3de6d505238d9afe4151b6e7
sudosurmrf <145930761+sudosurmrf@users.noreply.github.com>
2025-06-06 22:07:08 -0700

    updated static routes

--- backend/server.js
+++ backend/server.js
@@ -14,23 +14,23 @@ const __dirname = path.dirname(__filename);
 
 app.use(cors({origin: ['http://localhost:5173', 'https://were-so-back.com', 'https://defund-the-hoa.com']}))
 app.use(express.json());
-// app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static(path.join(__dirname, '../frontend/dist')))
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 
 app.use('/api', apiRouter);
 
 
 app.get(/^(?!\/api).*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   // res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.use((err, req,res,next) => {
   console.log(err);
   res.status(err.status || 500).json({error: err})
 })
-
-app.listen(process.env.PORT,() => {
+const PORT = process.env.PORT
+app.listen(PORT || 3000,() => {
   console.log(`listening on ${process.env.PORT}`)
 });
